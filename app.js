8f4ac9987ad8d7c8376e8ff0e66fa8a814f041aa
Bana <b.teklay@live.co.uk>
2026-07-08 15:33:09 +0100

    add import getMood function to chain the mood and temperature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { getLocation } from "./api/geo.js";
 import { getWeather } from "./api/weather.js"
+import { getMood } from "./data/moods.js";
 
 async function init() {
     const locationResult = await getLocation()
@@ -9,7 +10,18 @@ async function init() {
         const locationDiv = document.getElementById("location")
         locationDiv.textContent = `${locationResult.city}, ${locationResult.country}`
       const weatherResult = await getWeather(locationResult.latitude, locationResult.longitude)
-      console.log(weatherResult)
+
+     const condition = getMood(weatherResult.current.weather_code)
+    
+     const temperatureDiv = document.getElementById("temperature")
+     temperatureDiv.textContent = `${weatherResult.current.temperature_2m}°C`
+     
+     const moodDiv = document.getElementById("mood")
+     moodDiv.textContent = `${condition}`
+    
+
+     console.log(condition)
+     console.log(weatherResult)
     } else{
         const cityInfo = document.getElementById("cityInfo")
         cityInfo.style.display = "block"
