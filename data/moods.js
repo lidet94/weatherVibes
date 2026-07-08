cd4107295fe054393ee776f485ec22dcc8ce3968
Bana <b.teklay@live.co.uk>
2026-07-08 15:34:29 +0100

    create a getMood function with weathercode as a parameter to map the conditions of the 5 moods

---
+++ data/moods.js
@@ -0,0 +1,14 @@
+export function getMood(weatherCode){
+    
+    if(weatherCode === 0 || weatherCode === 1){
+        return "Sunny"
+    } else if(weatherCode === 2 || weatherCode === 3){
+        return "Partly cloudy"
+    } else if(weatherCode >= 51 && weatherCode <=67 || weatherCode >= 80 && weatherCode <=82){
+        return "Rainy"
+    } else if(weatherCode >= 71 && weatherCode <=77 || weatherCode >= 85 && weatherCode <=86){
+        return "Snowy"
+    } else if(weatherCode === 95 || weatherCode ===96 || weatherCode === 99){
+        return "Stormy"
+    } 
+}
