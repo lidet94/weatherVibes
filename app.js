2f6e9e1cb194dc54abb8d7d230341f65b4035368
Bana <b.teklay@live.co.uk>
2026-07-18 16:08:53 +0100

    add night mode detection, weather icons, feels like, humidity and applyTheme logic

--- app.js
+++ app.js
@@ -1,52 +1,84 @@
 import { getLocation } from "./api/geo.js";
 import { getMeals } from "./api/meals.js";
 import { getWeather } from "./api/weather.js"
-import { getMood, mealCategories } from "./data/moods.js";
+import { getMood, mealCategories, moodIcons } from "./data/moods.js";
+import { loadMusic } from "./api/youtube.js";
+
+function applyTheme(mood) {
+  document.body.className = `theme-${mood.toLowerCase().replace(' ', '-')}`
+}
 
 async function init() {
-    const locationResult = await getLocation()
-  
+  const locationResult = await getLocation()
 
-     if(locationResult){
-        const locationDiv = document.getElementById("location")
-        locationDiv.textContent = `${locationResult.city}, ${locationResult.country}`
-      const weatherResult = await getWeather(locationResult.latitude, locationResult.longitude)
 
-     const condition = getMood(weatherResult.current.weather_code)
-    
-     const temperatureDiv = document.getElementById("temperature")
-     temperatureDiv.textContent = `${weatherResult.current.temperature_2m}°C`
-     
-     const moodDiv = document.getElementById("mood")
-     moodDiv.textContent = `${condition}`
-     
+  if (locationResult) {
+    const locationDiv = document.getElementById("location")
+    locationDiv.textContent = `${locationResult.city}, ${locationResult.region}, ${locationResult.country}`
+    const weatherResult = await getWeather(locationResult.latitude, locationResult.longitude)
+
+    const condition = getMood(weatherResult.current.weather_code)
+
+    const temperatureDiv = document.getElementById("temperature")
+    temperatureDiv.textContent = `${weatherResult.current.temperature_2m}°C`
+
+    const moodDiv = document.getElementById("mood")
+
+    const moodIcon = document.getElementById("weatherIcon")
+
+    const app_temperature = document.getElementById("app_temp")
+    app_temperature.textContent = `Feels like: ${weatherResult.current.apparent_temperature}°C`
+
+    const relHumdity = document.getElementById("humidity")
+    relHumdity.textContent = `Humidity is ${weatherResult.current.relative_humidity_2m}%`
+
+    const isDay = weatherResult.current.is_day
+
+    if (isDay === 0) {
+      applyTheme("Night")
+      moodDiv.textContent = "Clear Night"
+      moodIcon.src = moodIcons["Night"]
+    } else {
+      applyTheme(condition)
+      moodDiv.textContent = condition
+      moodIcon.src = moodIcons[condition]
+
+    }
+
+
+    loadMusic(condition);
+
     const options = mealCategories[condition]
     const category = options[Math.floor(Math.random() * options.length)]
     const mealsResult = await getMeals(category)
-    console.log(mealsResult)
 
-    const shuffled = mealsResult.meals.sort(() => Math.random() - 0.5)
-    const threeMeals = shuffled.slice(0, 3)
 
-    const container = document.getElementById("mealsContainer")
+    if (mealsResult && mealsResult.meals) {
 
-    threeMeals.forEach(meal => {
-  console.log(meal.strMeal)
+      const shuffled = mealsResult.meals.sort(() => Math.random() - 0.5)
+      const threeMeals = shuffled.slice(0, 3)
 
-  const card = `<div class="card">
-  <img src="${meal.strMealThumb}" />
+      const container = document.getElementById("mealsContainer")
+
+      threeMeals.forEach(meal => {
+
+
+        const card = `<div class="card">
+ <a href="https://www.themealdb.com/meal/${meal.idMeal}" target="_blank"> <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/></a> 
   <h3>${meal.strMeal}</h3>
   <a href="https://www.themealdb.com/meal/${meal.idMeal}" target="_blank">View Recipe</a>
 </div>`
 
-container.innerHTML += card
-})
+        container.innerHTML += card
 
-     console.log(condition)
-     console.log(weatherResult)
-    } else{
-        const cityInfo = document.getElementById("cityInfo")
-        cityInfo.style.display = "block"
+      })
+    } else {
+      const container = document.getElementById("mealsContainer")
+      container.innerHTML = `<p class="error-msg">Could not load recipes right now. Try refreshing!</p>`;
     }
+  } else {
+    const cityInfo = document.getElementById("cityInfo")
+    cityInfo.style.display = "block"
+  }
 }
 init()
